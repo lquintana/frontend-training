ebbb63effcb31107c14ef75d1315b5d525a6f6c7
Leandro Quintana <leandro.quintana@mercadolibre.com>
2016-01-29 11:32:15 -0300

    dom events excersice

--- dom-events/exercises/exercise.js
+++ dom-events/exercises/exercise.js
@@ -3,3 +3,48 @@
 //     of the personality list
 // 2 - the browser must only redirect to the twitter handle if it is clicked
 //      on the personality selected (not on the list)
+'use strict';
+
+var personalitiesList = document.querySelector('#personalities');
+var personalitySelected = document.querySelector('#personalitySelected');
+
+function showPersonality(el) {
+
+    var personality = getPersonality(el);
+
+    personalitySelected.innerHTML = personality;
+}
+
+function getPersonality (el) {
+    var personality;
+
+    if (el.tagName === 'UL'){
+        personality = el.outerHTML;
+    }
+
+    if (el.tagName === 'LI') {
+        personality = el.parentNode.outerHTML;
+    }
+
+    if (el.tagName === 'A') {
+        personality = el.parentNode.parentNode.outerHTML;
+    }
+
+    return personality;
+
+}
+
+personalitiesList.addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    var element;
+
+    if (event.target != event.currentTarget) {
+        element = event.target;
+        showPersonality(element);
+    }
+
+    event.stopPropagation();
+
+}, false);
